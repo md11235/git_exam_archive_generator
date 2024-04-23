9299b6965d2736e808a97b3b52c44f638a2b51e1
zs <sen.zhang@qq.com>
2024-04-23 16:23:10 +0800

    忘记之前做了什么更新了。提交以便更新

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pandas as pd
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import numbers
 from openpyxl.styles import Font, Color, Border, Side
+import os
 import argparse
 
 
@@ -36,7 +37,8 @@ def print_hi(name):
 
 def extract_daily_performance(all_in_one_workbook_file, class_name, class_info_file,
                               output_template_workbook="05平时成绩模板.xlsx",
-                              output_start_row=5):
+                              output_start_row=5,
+                              num_attendances=10):
     df_daily_perf = pd.read_excel(all_in_one_workbook_file, sheet_name=DAILY_PERF, header=0)
 
     df_labs = pd.read_excel(all_in_one_workbook_file, sheet_name=LABS, header=0)
@@ -49,7 +51,7 @@ def extract_daily_performance(all_in_one_workbook_file, class_name, class_info_f
 
     print(df_stu_info)
 
-    df_index = df_stu_info[[0, 3]].rename({0: "学号", 3: "姓名"}, axis=1)
+    df_index = df_stu_info[[0, 1]].rename({0: "学号", 1: "姓名"}, axis=1)
     # for att_index in range(1, NO_OF_ATTENDANCE+1):
     #     df_result[ATTENDANCE+str(att_index)] = df_daily_perf
     # print(df_result)
@@ -63,7 +65,8 @@ def extract_daily_performance(all_in_one_workbook_file, class_name, class_info_f
     print("--------")
     df_result = df_daily_perf.loc[df_index.index].reset_index()
 
-    template_wb = openpyxl.load_workbook(output_template_workbook)
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    template_wb = openpyxl.load_workbook(os.path.join(script_dir, output_template_workbook))
 
     template_ws = None
     if DAILY_PERF in template_wb:
@@ -88,35 +91,33 @@ def extract_daily_performance(all_in_one_workbook_file, class_name, class_info_f
         template_ws["E{}".format(row_id)] = row["班级"]
         template_ws["E{}".format(row_id)].font = cell_font
 
-        template_ws["I{}".format(row_id)] = row["考勤1"]
-        template_ws["K{}".format(row_id)] = row["考勤2"]
-        template_ws["L{}".format(row_id)] = row["考勤3"]
-        template_ws["M{}".format(row_id)] = row["考勤4"]
-        template_ws["N{}".format(row_id)] = row["考勤5"]
-        template_ws["O{}".format(row_id)] = row["考勤6"]
-        template_ws["P{}".format(row_id)] = row["考勤7"]
-        template_ws["Q{}".format(row_id)] = row["考勤8"]
-        template_ws["R{}".format(row_id)] = row["考勤9"]
-        template_ws["S{}".format(row_id)] = row["考勤10"]
-        template_ws["T{}".format(row_id)] = row["考勤分"]
-        template_ws["U{}".format(row_id)] = row["实验"]
-        template_ws["V{}".format(row_id)] = row["课堂表现"]
-        template_ws["W{}".format(row_id)] = row["总成绩"]
+        template_ws["{}{}".format('I', row_id)] = row["考勤{}".format(1)]
+        attendance_first_column_ord = ord("K")
+        for i in range(1, num_attendances):
+            template_ws["{}{}".format(chr(attendance_first_column_ord+i), row_id)] = row["考勤{}".format(i+1)]
+
+        template_ws["{}{}".format(chr(attendance_first_column_ord+num_attendances), row_id)] = row["考勤分"]
+        template_ws["{}{}".format(chr(attendance_first_column_ord+num_attendances+1), row_id)] = row["实验"]
+        template_ws["{}{}".format(chr(attendance_first_column_ord+num_attendances+2), row_id)] = row["课堂表现"]
+        template_ws["{}{}".format(chr(attendance_first_column_ord+num_attendances+3), row_id)] = row["总成绩"]
+
+        final_column = chr(attendance_first_column_ord+num_attendances+3)
 
         # TODO: merge E F G H
 
-        set_border(template_ws, "A{}:W{}".format(row_id, row_id))
+        set_border(template_ws, "A{}:{}{}".format(row_id, final_column, row_id))
 
     # for row in dataframe_to_rows(df_result, index=False, header=False):
     #     print(row)
     #     # template_ws.append(row)
 
-    template_wb.save("../05_gen_平时成绩_{}.xlsx".format(class_name))
+    template_wb.save("05_gen_平时成绩_{}.xlsx".format(class_name))
 
 
 def extract_class_performance(all_in_one_workbook_file, class_name, class_info_file,
                               output_template_workbook="05平时成绩模板.xlsx",
-                              output_start_row=4):
+                              output_start_row=4,
+                              num_chapters=10):
     df_lecture_perf = pd.read_excel(all_in_one_workbook_file, sheet_name=LECTURE_PERF, header=0)
     print(df_lecture_perf)
 
@@ -124,7 +125,7 @@ def extract_class_performance(all_in_one_workbook_file, class_name, class_info_f
 
     print(df_stu_info)
 
-    df_index = df_stu_info[[0, 3]].rename({0: "学号", 3: "姓名"}, axis=1)
+    df_index = df_stu_info[[0, 1]].rename({0: "学号", 1: "姓名"}, axis=1)
     # for att_index in range(1, NO_OF_ATTENDANCE+1):
     #     df_result[ATTENDANCE+str(att_index)] = df_daily_perf
     # print(df_result)
@@ -136,7 +137,8 @@ def extract_class_performance(all_in_one_workbook_file, class_name, class_info_f
     print("--------")
     df_result = df_lecture_perf.loc[df_index.index].reset_index()
 
-    template_wb = openpyxl.load_workbook(output_template_workbook)
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    template_wb = openpyxl.load_workbook(os.path.join(script_dir, output_template_workbook))
 
     template_ws = None
     if LECTURE_PERF in template_wb:
@@ -148,6 +150,9 @@ def extract_class_performance(all_in_one_workbook_file, class_name, class_info_f
     print(df_result)
 
     cell_font = Font(name="SimSun", size=11)
+
+    chap_id2name = {14: "第15章",
+                    15: "第17章"}
 
     for ind, row in df_result.iterrows():
         row_id = output_start_row + ind
@@ -161,25 +166,24 @@ def extract_class_performance(all_in_one_workbook_file, class_name, class_info_f
         template_ws["C{}".format(row_id)] = row["姓名"]
         template_ws["C{}".format(row_id)].font = cell_font
 
-        template_ws["D{}".format(row_id)] = row["第一章"]
+        # template_ws["D{}".format(row_id)] = row["第1章"]
         # template_ws["D{}".format(row_id)].font = cell_font
 
-        template_ws["E{}".format(row_id)] = row["第二章"]
-        template_ws["F{}".format(row_id)] = row["第三章"]
-        template_ws["G{}".format(row_id)] = row["第四章"]
-        template_ws["H{}".format(row_id)] = row["第五章"]
-        template_ws["I{}".format(row_id)] = row["第六章"]
-        template_ws["J{}".format(row_id)] = row["第七章"]
-        template_ws["K{}".format(row_id)] = row["第八章"]
-        template_ws["L{}".format(row_id)] = row["总分"]
+        chapter_first_col_ord = ord('D')
+        for i in range(num_chapters):
+            chap_name = chap_id2name[i+1] if (i+1) in chap_id2name else "第{}章".format(i+1)
+            template_ws["{}{}".format(chr(chapter_first_col_ord+i), row_id)] = row[chap_name]
 
-        set_border(template_ws, "A{}:M{}".format(row_id, row_id))
+        template_ws["{}{}".format(chr(chapter_first_col_ord+num_chapters), row_id)] = row["总分"]
+        final_col = chr(chapter_first_col_ord+num_chapters+1)
+
+        set_border(template_ws, "A{}:{}{}".format(row_id, final_col, row_id))
 
     # for row in dataframe_to_rows(df_result, index=False, header=False):
     #     print(row)
     #     # template_ws.append(row)
 
-    template_wb.save("../05_gen_课堂表现_{}.xlsx".format(class_name))
+    template_wb.save("05_gen_课堂表现_{}.xlsx".format(class_name))
 
 
 # Press the green button in the gutter to run the script.
@@ -192,36 +196,42 @@ if __name__ == '__main__':
     arg_parser.add_argument("class_students_id_name_filepath",
                             help="包含班级里学生的学号、姓名的tsv文件路径")
 
+    arg_parser.add_argument("-t", help="输出模板", dest="output_template_filepath")
+
     args = arg_parser.parse_args()
 
     extract_daily_performance(args.all_in_one_scores_file,
                               args.class_name,
-                              args.class_students_id_name_filepath)
+                              args.class_students_id_name_filepath,
+                              output_template_workbook=args.output_template_filepath,
+                              num_attendances=9)
 
     extract_class_performance(args.all_in_one_scores_file,
                               args.class_name,
-                              args.class_students_id_name_filepath)
+                              args.class_students_id_name_filepath,
+                              output_template_workbook=args.output_template_filepath,
+                              num_chapters=15)
 
 
-    extract_daily_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
-                              "大数据213",
-                              "../学生名单_大数据213.csv")
-    extract_daily_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
-                              "网络211",
-                              "../学生名单_网络211.csv")
-
-    extract_daily_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
-                              "网络212",
-                              "../学生名单_网络212.csv")
-    extract_class_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
-                              "大数据213",
-                              "../学生名单_大数据213.csv")
-
-    extract_class_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
-                          "网络211",
-                          "../学生名单_网络211.csv")
-    extract_class_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
-                          "网络212",
-                          "../学生名单_网络212.csv")
+    # extract_daily_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
+    #                           "大数据213",
+    #                           "../学生名单_大数据213.csv")
+    # extract_daily_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
+    #                           "网络211",
+    #                           "../学生名单_网络211.csv")
+    #
+    # extract_daily_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
+    #                           "网络212",
+    #                           "../学生名单_网络212.csv")
+    # extract_class_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
+    #                           "大数据213",
+    #                           "../学生名单_大数据213.csv")
+    #
+    # extract_class_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
+    #                       "网络211",
+    #                       "../学生名单_网络211.csv")
+    # extract_class_performance("../05-平时成绩-all3classes_v2_归档.xlsx",
+    #                       "网络212",
+    #                       "../学生名单_网络212.csv")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
